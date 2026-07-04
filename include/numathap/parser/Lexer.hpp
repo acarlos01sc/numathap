b0956d24d76d97787e6d6b20842038421d60b23d
Antonio Carlos <acarlos01.sc@gmail.com>
2026-07-04 20:42:57 -0300

    clang-format

---
+++ include/numathap/parser/Lexer.hpp
@@ -0,0 +1,28 @@
+#include <cstddef>
+#include <string>
+#include <vector>
+
+#include "Token.hpp"
+
+namespace numathap::parser {
+
+class Lexer {
+    public:
+        explicit Lexer(const std::string &input);
+        Token next();
+        std::vector<Token> tokenize();
+    private:
+        char peek() const;
+        char get();
+        void skip_white_space();
+
+        Token read_numer();
+        Token read_identifier();
+
+        const std::string &input_;
+        size_t pos_;
+        size_t line_;
+        size_t column_;
+};
+
+} // namespace numathap::parser
