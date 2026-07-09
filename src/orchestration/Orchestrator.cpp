b11afbddf83c5d6584b75af11c5220727ac8c072
Antonio Carlos <acarlos01.sc@gmail.com>
2026-07-09 17:17:11 -0300

    Add initial Orchestrator component

---
+++ src/orchestration/Orchestrator.cpp
@@ -0,0 +1,10 @@
+#include "numathap/orchestration/Orchestrator.hpp"
+
+namespace numathap::orchestration {
+
+ast::NodePtr Orchestrator::prepare(ast::NodePtr root)
+{
+    return root;
+}
+
+}  // namespace numathap::orchestration
